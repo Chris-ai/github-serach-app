346bb2274917c65afcef209141fca0d4e4f4b60b
chris-ai <krzysiek.kondracikowski@gmail.com>
2023-11-03 19:06:30 +0100

    Create custom project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Space_Mono } from 'next/font/google'
+import './globals.css'
+
+const spaceMono = Space_Mono({weight: ['400','700']})
+
+export const metadata: Metadata = {
+  title: 'Github Search App',
+  description: 'Frontend Mentor Challenge',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={spaceMono.className}>{children}</body>
+    </html>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        azure: "#0079FF",
+        bluegray: "#697C9A",
+        steelblue: "#4B6A9B",
+        midnightblue: "#2B3442",
+        lavender: "#F6F8FF",
+        offwhite: "#FEFEFE",
+        navyblue: "#141D2F",
+        deepblue: "#1E2A47",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
